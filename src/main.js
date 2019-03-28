bee2a31457b7e607920c1382fc598ddfc3ec9955
HippySir <737438933@qq.com>
2019-03-28 16:09:49 +0800

    轮播图功能

--- src/main.js
+++ src/main.js
@@ -19,6 +19,20 @@ import axios from 'axios';
 Vue.prototype.$axios = axios;
 // 设置基地址
 axios.defaults.baseURL = 'http://111.230.232.110:8899';
+// 引入commit文件
+ import moment from 'moment';
+// 设置全局的过滤器
+Vue.filter('globalFilters',function(value,fltemplate){
+  if(fltemplate){
+    return moment(value).format(fltemplate);
+  }else{
+    return moment(value).format('YYYY-MM-DD');
+  }
+})
+// 安装饿了吗MIUI
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+Vue.use(ElementUI);
 // 设置路由规则
 const routes = [
   {path:'/',component:index },
